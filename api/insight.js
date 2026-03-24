5d5e91cf993695c7de108ccf7cd7ec8ec7ff25db
kabeerkp <ecowattenergyhabittracker@gmail.com>
2026-03-24 11:21:08 +0530

    changed gemini version and enable bug print

--- api/insight.js
+++ api/insight.js
@@ -13,7 +13,7 @@ module.exports = async function handler(req, res) {
 
     const options = {
       hostname: 'generativelanguage.googleapis.com',
-      path: `/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      path: `/v1beta/models/gemini-2-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
